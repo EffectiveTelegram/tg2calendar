d6737c89fc4027ec0cf430ad6c2010815f293acc
egorkuzn2002 <georkuzn2002@mail.ru>
2024-08-22 15:16:58 +0700

    initial commit

---
+++ src/main/java/com/github/effective_telegram/tg2calendar/Tg2calendarApplication.java
@@ -0,0 +1,13 @@
+package com.github.effective_telegram.tg2calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tg2calendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tg2calendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/effective_telegram/tg2calendar/Tg2calendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.effective_telegram.tg2calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tg2calendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
